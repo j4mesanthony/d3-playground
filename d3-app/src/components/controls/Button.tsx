900a17dd5b66cc5a5f7a0337e9c39b004a610e64
James Anthony <janthony@greenleaf.com.au>
2023-12-15 12:13:02 +1000

    feat: Button styling edits

--- d3-app/src/components/controls/Button.tsx
+++ d3-app/src/components/controls/Button.tsx
@@ -4,6 +4,6 @@ type Props = {
 
 export function Button({ label }: Props) {
     return (
-        <button className='bg-cyan-500 text-neutral-50 hover:bg-cyan-400 rounded-md  p-2 pl-4 pr-4'>{ label }</button>
+        <button className='bg-cyan-500 text-neutral-50 hover:bg-cyan-400 rounded-full min-w-[90px] justify-center w-fit items-center flex p-2 pl-4 pr-4'>{ label }</button>
     )
 }
