f98ab043eab762ef2d0dd7374c4ed2723bffabb2
James Anthony <janthony@greenleaf.com.au>
2023-12-15 12:01:30 +1000

    build: integrated tailwind styles for storybook

--- d3-app/src/components/controls/Button.tsx
+++ d3-app/src/components/controls/Button.tsx
@@ -4,6 +4,6 @@ type Props = {
 
 export function Button({ label }: Props) {
     return (
-        <button>{ label }</button>
+        <button className='bg-cyan-500 text-neutral-50 hover:bg-cyan-400 rounded-md  p-2 pl-4 pr-4'>{ label }</button>
     )
 }
